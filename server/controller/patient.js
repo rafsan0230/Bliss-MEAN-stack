31ced69e642d5c5325cd94b4c57ef6fb5fedeb93
rafsan0230 <rafsan0230@gmail.com>
2023-03-02 19:14:34 +0600

    dataflow done

--- server/controller/patient.js
+++ server/controller/patient.js
@@ -2,7 +2,6 @@ const Patient = require('./../models/patient');
 
 
 const getPatients = async (req, res) => {
-  console.log("some");
   try {
     const patient = await Patient.find();
     res.status(201);
@@ -14,10 +13,10 @@ const getPatients = async (req, res) => {
 }
 
 const postPatient = async (req, res,) => {
-  console.log(req.body)
   if (req.body.email) {
     try {
       const result = await Patient.create(req.body);
+      console.log(req.body)
       res.status(201);
       res.send(result);
       return result;
